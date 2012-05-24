a86c8cb4de817eb156937be41e235c0de9d4442b
Andrew Hautau <arhautau@gmail.com>
2012-05-23 20:04:20 -0700

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SeattleBeerClub::Application.config.secret_token = 'a73e622115025fc11722634ea6407e148f00fe30e25e48dc0a097216273549c6bbe3c78a2cda64d7d4e7a1e55c66ea99f7fe5f46a107d398fa984cfcd03b9b4b'
